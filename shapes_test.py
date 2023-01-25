362b5fd73f55828d7dfb09736c8b0a64cd429c4b
vijay pandurangan <vijayp@vijayp.ca>
2023-01-24 23:13:01 -0500

    fix a copule of comments.

--- shapes_test.py
+++ shapes_test.py
@@ -11,13 +11,18 @@ import os.path
 
 class TestShapes(unittest.TestCase):
     def _compare_canvas_to_expected(self, expected_filename):
-        ''' compares the current canvas to an expected filename.
+        ''' compares the current canvas to an expected file.
         Returns None if and only if the files are identical'''
+
         TOLERANCE = 1.0 # somewhere between 0 and 255, higher is more lax.
+
         with tempfile.TemporaryDirectory() as tmp_dirname:
             actual_ps = os.path.join(tmp_dirname, 'canvas.ps')
             actual_png = os.path.join(tmp_dirname, 'canvas.png')
             canvas = turtle.getcanvas()
+            
+            # canvas generates a postscript file, but we have to convert it to a png in
+            # order to compare it using matplotlib's library
             canvas.postscript(file=actual_ps)
             with Image.open(actual_ps) as im:
                 im.save(actual_png)
@@ -25,12 +30,15 @@ class TestShapes(unittest.TestCase):
 
 
     def setUp(self):
+        # this is run before every test
         turtle.reset()
 
     def test_circle(self):
         t = turtle.getturtle()
         shapes.draw_circle(t,20,20,20)
         t.hideturtle()
+
+        # compare this 20,20,20 turtle against the well-known turtle png
         self.assertIsNone(self._compare_canvas_to_expected(expected_filename='testdata/circle-20.png'))
 
     def test_circle_fail(self):
@@ -38,8 +46,9 @@ class TestShapes(unittest.TestCase):
         t = turtle.getturtle()
         shapes.draw_circle(t,20,20,29)
         t.hideturtle()
-        self.assertIsNotNone(self._compare_canvas_to_expected(expected_filename='testdata/circle-20.png'))
 
+        # this should not match, therefore should be not none.
+        self.assertIsNotNone(self._compare_canvas_to_expected(expected_filename='testdata/circle-20.png'))
 
 
 if __name__ == '__main__':
